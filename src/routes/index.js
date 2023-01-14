e1c16e14703efb59341ab130f19e7c6dee551216
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-01-13 20:23:46 -0500

    servicios

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,32 @@ router.get('/', (req, res) => {
     res.render('./partials/home');
 });
 
+router.get('/servicios', (req, res) => {
+    res.render('./partials/servicios');
+});
+
+router.get('/servicios/diseno', (req, res) => {
+    res.render('./services/diseno');
+});
+
+router.get('/servicios/desarrollo', (req, res) => {
+    res.render('./services/desarrollo');
+});
+
+router.get('/servicios/marketing', (req, res) => {
+    res.render('./services/marketing');
+});
+
+router.get('/servicios/edicion', (req, res) => {
+    res.render('./services/edicion');
+});
+
+router.get('/servicios/blockchain', (req, res) => {
+    res.render('./services/blockchain');
+});
+
+router.get('/contacto', (req, res) => {
+    res.render('./partials/contacto');
+});
+
 module.exports = router;
