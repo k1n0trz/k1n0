c1cd011babd483706c800e92e0ea7f1674ecc392
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-01-17 15:26:03 -0500

    sitemap 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,7 +42,7 @@ router.get('/politica-privacidad', (req, res) => {
 });
 
 router.get('/sitemap', (req, res) => {
-    res.render('./partials/sitemap');
+    res.render('./partials/sitemap.xml');
 });
 
 module.exports = router;
