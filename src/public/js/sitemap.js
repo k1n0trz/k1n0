207619361f1a75bcea4af71ad140223717aaa10d
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-01-17 14:29:56 -0500

    gtags

---
+++ src/public/js/sitemap.js
@@ -0,0 +1,44 @@
+{
+    "urlset"; {
+      "-xmlns"; "http://www.sitemaps.org/schemas/sitemap/0.9",
+      "-xmlns:xsi"; "http://www.w3.org/2001/XMLSchema-instance",
+      "-xsi:schemaLocation"; "http://www.sitemaps.org/schemas/sitemap/0.9             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd",
+      "url"; [
+        {
+          "loc": "https://k1n0.herokuapp.com/",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "1.00"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/nosotros",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/servicios",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/signin",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/signup",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/cookies",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        },
+        {
+          "loc": "https://k1n0.herokuapp.com/privacy",
+          "lastmod": "2021-01-28T02:11:30+00:00",
+          "priority": "0.80"
+        }
+      ]
+    }
+  }
