c2fad25895bd4ef16e522f6afc92d92c71665331
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-01-18 09:03:23 -0500

    ajustes varios

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-module.exports = {
-    database: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'test'
-    }
-};
-
 // module.exports = {
 //     database: {
-//         host: 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'e0lwlm13xfcrsty5',
-//         password: 'oyct0qyoqhhet8qz',
-//         database: 'snzxe6ux5pzgm3ao'
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         database: 'test'
 //     }
 // };
+
+module.exports = {
+    database: {
+        host: 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'e0lwlm13xfcrsty5',
+        password: 'oyct0qyoqhhet8qz',
+        database: 'snzxe6ux5pzgm3ao'
+    }
+};
